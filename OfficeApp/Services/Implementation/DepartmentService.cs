e1ab84a56f572c0bbc1f0f86b4c6ce725d628f79
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-06 13:25:25 +0545

    Added concrete representation methods for support of Department related business logic.

---
+++ OfficeApp/Services/Implementation/DepartmentService.cs
@@ -0,0 +1,48 @@
+﻿using OfficeApp.Models;
+using OfficeApp.Services.Abstraction;
+
+namespace OfficeApp.Services.Implementation;
+
+public class DepartmentService : IDepartmentService
+{
+
+    private readonly AppDbContext _context;
+
+    public DepartmentService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void AddDepartment(Department department)
+    {
+        _context.Departments.Add(department);
+        _context.SaveChanges();
+    }
+
+    public void DeleteDepartment(Department department)
+    {
+        _context.Departments.Remove(department);
+        _context.SaveChanges();
+    }
+
+    public bool DepartmentExists(int Id)
+    {
+        return _context.Departments.Any(x => x.Id == Id);
+    }
+
+    public Department GetDepartmentById(int? Id)
+    {
+        return _context.Departments.Find(Id);
+    }
+
+    public List<Department> GetDepartments()
+    {
+        return _context.Departments.ToList();
+    }
+
+    public void UpdateDepartment(Department department)
+    {
+        _context.Departments.Update(department);
+        _context.SaveChanges();
+    }
+}
