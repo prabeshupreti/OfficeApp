060d40e161b6d6bd4409f52b6c9cb3993cf8d986
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-06 13:25:08 +0545

    Added signature methods for support of Department related business logic.

---
+++ OfficeApp/Services/Abstraction/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using OfficeApp.Models;
+
+namespace OfficeApp.Services.Abstraction;
+
+public interface IDepartmentService
+{
+    void AddDepartment(Department department);
+    void DeleteDepartment(Department department);
+    List<Department> GetDepartments();
+    Department GetDepartmentById(int? Id);
+    void UpdateDepartment(Department department);
+    bool DepartmentExists(int Id);
+}
