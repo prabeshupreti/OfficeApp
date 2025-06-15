29402a4d06ca44257f55a709d4685aecc9537e27
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-15 13:30:06 +0545

    Added host support abstraction part.

---
+++ OfficeApp/Services/Abstraction/IWebHostEnvironmentService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OfficeApp.Services.Abstraction;
+
+public interface IWebHostEnvironmentService
+{
+    (string AbsolutePath, string RelativePath) GetFilePath(string FileName);
+    void AddEmployeeProfilePhoto(IFormFile image, string FilePath);
+    void DeleteEmployeeProfilePhoto(string PhotoPath);
+}
