9d69047466f746a05455f46d992d7d064a272a8a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-15 13:29:45 +0545

    Minor bug fixes.

---
+++ OfficeApp/Services/Abstraction/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using OfficeApp.Models;
+
+namespace OfficeApp.Services.Abstraction;
+
+public interface IEmployeeService
+{
+    void AddEmployee(Employee employee);
+    void DeleteEmployee(Employee employee);
+    List<Employee> GetEmployees();
+    IQueryable<Employee> GetAllEmployees();
+    Employee GetEmployeeById(int? Id);
+    void UpdateEmployee(Employee employee);
+    bool EmployeeExists(int? Id);
+}
