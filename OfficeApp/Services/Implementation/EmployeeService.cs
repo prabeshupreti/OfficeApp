dd5263f48eaff17d556c9235f244d700ebc49d3a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-15 13:30:50 +0545

    Added Employee Service implementation part.

---
+++ OfficeApp/Services/Implementation/EmployeeService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfficeApp.Models;
+using OfficeApp.Services.Abstraction;
+
+namespace OfficeApp.Services.Implementation;
+
+public class EmployeeService : IEmployeeService
+{
+    private readonly IWebHostEnvironment _environment;
+
+    private readonly AppDbContext _context;
+
+    public EmployeeService(IWebHostEnvironment environment, AppDbContext context)
+    {
+        _environment = environment;
+        _context = context;
+    }
+
+    public void AddEmployee(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        _context.SaveChanges();
+    }
+
+    public void DeleteEmployee(Employee employee)
+    {
+        _context.Employees.Remove(employee);
+        _context.SaveChanges();
+    }
+
+    public bool EmployeeExists(int? Id)
+    {
+        return _context.Employees.Any(x => x.Id == Id);
+    }
+
+    public IQueryable<Employee> GetAllEmployees()
+    {
+        return _context.Employees.Include(x => x.Department);
+    }
+
+    public Employee GetEmployeeById(int? Id)
+    {
+        return _context.Employees.Find(Id);
+    }
+
+    public List<Employee> GetEmployees()
+    {
+        return _context.Employees.Include(x => x.Department).ToList();
+    }
+
+    public void UpdateEmployee(Employee employee)
+    {
+        _context.Update(employee);
+        _context.SaveChanges();
+    }
+}
