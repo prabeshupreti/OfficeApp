517b1657c6ca952c2deaf1f1de4b21e8b72b6d56
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-15 13:31:44 +0545

    Added dependency registration for employee service and host support service.

--- OfficeApp/Program.cs
+++ OfficeApp/Program.cs
@@ -9,12 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(config => 
+builder.Services.AddDbContext<AppDbContext>(config =>
 {
     config.UseSqlite(builder.Configuration.GetConnectionString("Default"));
 });
 
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddTransient<IWebHostEnvironmentService, WebHostEnvironmentService>();
 
 var app = builder.Build();
 
@@ -24,13 +26,17 @@ var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
 dbContext.Database.Migrate();
 
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment()) 
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
