dc7279b5ecc640769add37dfeac587527d75ba26
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-16 08:15:22 +0545

    Added Authorization.

--- OfficeApp/Controllers/DepartmentController.cs
+++ OfficeApp/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OfficeApp.Models;
@@ -7,6 +8,7 @@ using OfficeApp.Services.Implementation;
 
 namespace OfficeApp.Controllers;
 
+[Authorize]
 public class DepartmentController : Controller
 {
     private readonly IDepartmentService _departmentService;
--- OfficeApp/Controllers/EmployeeController.cs
+++ OfficeApp/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,209 +13,209 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OfficeApp.Controllers
+namespace OfficeApp.Controllers;
+
+[Authorize]
+public class EmployeeController : Controller
 {
-    public class EmployeeController : Controller
+    private readonly IWebHostEnvironmentService _webHostEnvironmentService;
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IWebHostEnvironmentService webHostEnvironmentService,
+        IEmployeeService employeeService)
     {
-        private readonly IWebHostEnvironmentService _webHostEnvironmentService;
-        private readonly IEmployeeService _employeeService;
+        _webHostEnvironmentService = webHostEnvironmentService;
+        _employeeService = employeeService;
+    }
 
-        public EmployeeController(IWebHostEnvironmentService webHostEnvironmentService,
-            IEmployeeService employeeService)
+    // GET: Employee
+    public IActionResult Index()
+    {
+        return View(_employeeService.GetEmployees());
+    }
+
+    // GET: Employee/Details/5
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
         {
-            _webHostEnvironmentService = webHostEnvironmentService;
-            _employeeService = employeeService;
+            return NotFound();
         }
 
-        // GET: Employee
-        public IActionResult Index()
+        var employee = _employeeService.GetEmployeeById(id);
+        if (employee == null)
         {
-            return View(_employeeService.GetEmployees());
+            return NotFound();
         }
 
-        // GET: Employee/Details/5
-        public IActionResult Details(int? id)
+        return View(employee);
+    }
+
+    // GET: Employee/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(EmployeeViewModel employeeViewModel)
+    {
+        if (employeeViewModel.Photo != null && employeeViewModel.Photo.Length > 0)
         {
-            if (id == null)
+            // Create a unique filename (optional)
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(employeeViewModel.Photo.FileName);
+
+            Employee employee = new Employee
             {
-                return NotFound();
+                FirstName = employeeViewModel.FirstName,
+                LastName = employeeViewModel.LastName,
+                Contact = employeeViewModel.Contact,
+                Address = employeeViewModel.Address,
+                DepartmentId = employeeViewModel.DepartmentId
+            };
+
+            var FilePath = _webHostEnvironmentService.GetFilePath(fileName);
+
+            employee.PhotoPath = FilePath.RelativePath;
+
+            if (!ModelState.IsValid)
+            {
+                return View(employeeViewModel);
             }
 
-            var employee = _employeeService.GetEmployeeById(id);
-            if (employee == null)
+            _employeeService.AddEmployee(employee);
+            _webHostEnvironmentService.AddEmployeeProfilePhoto(employeeViewModel.Photo, FilePath.AbsolutePath);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        ModelState.AddModelError(nameof(employeeViewModel.Photo), "Please, add a photo.");
+        return View(employeeViewModel);
+    }
+
+    // GET: Employee/Edit/5
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var employeeExists = _employeeService.EmployeeExists(id);
+        if (!employeeExists)
+        {
+            return NotFound();
+        }
+
+        var employee = _employeeService.GetEmployeeById(id);
+
+        UpdateEmployeeViewModel updateEmployeeViewModel = new UpdateEmployeeViewModel
+        {
+            Id = employee!.Id,
+            FirstName = employee.FirstName,
+            LastName = employee.LastName,
+            Contact = employee.Contact,
+            Address = employee.Address,
+            PhotoPath = employee.PhotoPath,
+            DepartmentId = employee.DepartmentId,
+        };
+
+        return View(updateEmployeeViewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, UpdateEmployeeViewModel employeeViewModel)
+    {
+        if (id != employeeViewModel.Id)
+        {
+            return NotFound();
+        }
+        try
+        {
+            var employee = new Employee
             {
-                return NotFound();
-            }
+                Id = employeeViewModel.Id,
+                FirstName = employeeViewModel.FirstName,
+                LastName = employeeViewModel.LastName,
+                Contact = employeeViewModel.Contact,
+                Address = employeeViewModel.Address,
+                DepartmentId = employeeViewModel.DepartmentId,
+            };
 
-            return View(employee);
-        }
-
-        // GET: Employee/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(EmployeeViewModel employeeViewModel)
-        {
             if (employeeViewModel.Photo != null && employeeViewModel.Photo.Length > 0)
             {
                 // Create a unique filename (optional)
                 var fileName = Guid.NewGuid().ToString() + Path.GetExtension(employeeViewModel.Photo.FileName);
 
-                Employee employee = new Employee
-                {
-                    FirstName = employeeViewModel.FirstName,
-                    LastName = employeeViewModel.LastName,
-                    Contact = employeeViewModel.Contact,
-                    Address = employeeViewModel.Address,
-                    DepartmentId = employeeViewModel.DepartmentId
-                };
-
+                // Determine the path to save file (e.g., wwwroot/ProfilePhotos)
                 var FilePath = _webHostEnvironmentService.GetFilePath(fileName);
 
-                employee.PhotoPath = FilePath.RelativePath;
+                string AbsoluteFilePath = FilePath.AbsolutePath;
+
+                string RelativeFilePath = FilePath.RelativePath;
 
                 if (!ModelState.IsValid)
                 {
                     return View(employeeViewModel);
                 }
 
-                _employeeService.AddEmployee(employee);
-                _webHostEnvironmentService.AddEmployeeProfilePhoto(employeeViewModel.Photo, FilePath.AbsolutePath);
+                employee.PhotoPath = RelativeFilePath;
 
-                return RedirectToAction(nameof(Index));
+                _webHostEnvironmentService.AddEmployeeProfilePhoto(employeeViewModel.Photo, AbsoluteFilePath);
+
+                _webHostEnvironmentService.DeleteEmployeeProfilePhoto(employeeViewModel.PhotoPath);
+            }
+            else
+            {
+                employee.PhotoPath = employeeViewModel.PhotoPath;
             }
 
-            ModelState.AddModelError(nameof(employeeViewModel.Photo), "Please, add a photo.");
-            return View(employeeViewModel);
-        }
-
-        // GET: Employee/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employeeExists = _employeeService.EmployeeExists(id);
-            if (!employeeExists)
-            {
-                return NotFound();
-            }
-
-            var employee = _employeeService.GetEmployeeById(id);
-
-            UpdateEmployeeViewModel updateEmployeeViewModel = new UpdateEmployeeViewModel
-            {
-                Id = employee!.Id,
-                FirstName = employee.FirstName,
-                LastName = employee.LastName,
-                Contact = employee.Contact,
-                Address = employee.Address,
-                PhotoPath = employee.PhotoPath,
-                DepartmentId = employee.DepartmentId,
-            };
-
-            return View(updateEmployeeViewModel);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(int id, UpdateEmployeeViewModel employeeViewModel)
-        {
-            if (id != employeeViewModel.Id)
-            {
-                return NotFound();
-            }
-            try
-            {
-                var employee = new Employee
-                {
-                    Id = employeeViewModel.Id,
-                    FirstName = employeeViewModel.FirstName,
-                    LastName = employeeViewModel.LastName,
-                    Contact = employeeViewModel.Contact,
-                    Address = employeeViewModel.Address,
-                    DepartmentId = employeeViewModel.DepartmentId,
-                };
-
-                if (employeeViewModel.Photo != null && employeeViewModel.Photo.Length > 0)
-                {
-                    // Create a unique filename (optional)
-                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(employeeViewModel.Photo.FileName);
-
-                    // Determine the path to save file (e.g., wwwroot/ProfilePhotos)
-                    var FilePath = _webHostEnvironmentService.GetFilePath(fileName);
-
-                    string AbsoluteFilePath = FilePath.AbsolutePath;
-
-                    string RelativeFilePath = FilePath.RelativePath;
-
-                    if (!ModelState.IsValid)
-                    {
-                        return View(employeeViewModel);
-                    }
-
-                    employee.PhotoPath = RelativeFilePath;
-
-                    _webHostEnvironmentService.AddEmployeeProfilePhoto(employeeViewModel.Photo, AbsoluteFilePath);
-
-                    _webHostEnvironmentService.DeleteEmployeeProfilePhoto(employeeViewModel.PhotoPath);
-                }
-                else
-                {
-                    employee.PhotoPath = employeeViewModel.PhotoPath;
-                }
-
-                _employeeService.UpdateEmployee(employee);
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!_employeeService.EmployeeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
-
-        // GET: Employee/Delete/5
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employee = _employeeService.GetEmployeeById(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return View(employee);
-        }
-
-        // POST: Employee/Delete/5
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var employee = _employeeService.GetEmployeeById(id);
-            if (employee != null)
-                _employeeService.DeleteEmployee(employee);
-
-
-            // Determine the path to save file (e.g., wwwroot/ProfilePhotos)
-            _webHostEnvironmentService.DeleteEmployeeProfilePhoto(employee!.PhotoPath);
+            _employeeService.UpdateEmployee(employee);
 
             return RedirectToAction(nameof(Index));
         }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_employeeService.EmployeeExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+
+    // GET: Employee/Delete/5
+    public IActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var employee = _employeeService.GetEmployeeById(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        return View(employee);
+    }
+
+    // POST: Employee/Delete/5
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var employee = _employeeService.GetEmployeeById(id);
+        if (employee != null)
+            _employeeService.DeleteEmployee(employee);
+
+
+        // Determine the path to save file (e.g., wwwroot/ProfilePhotos)
+        _webHostEnvironmentService.DeleteEmployeeProfilePhoto(employee!.PhotoPath);
+
+        return RedirectToAction(nameof(Index));
     }
 }
