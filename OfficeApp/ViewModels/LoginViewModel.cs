6b7c8d32ca2a39e2c13849de313623691187d819
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-16 08:16:45 +0545

    Login View Model for login support.

---
+++ OfficeApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OfficeApp.ViewModels;
+
+public class LoginViewModel
+{
+    [Display(Name = "Email or Username")]
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
