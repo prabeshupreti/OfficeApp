5e26d3316a8417786f6fe76cf66bfed05363bf5a
Prabesh Upreti <prabeshupreti@ismt.edu.np>
2025-06-16 08:16:24 +0545

    Added Account Controller to Support Identity for Authentication and Authorization.

---
+++ OfficeApp/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OfficeApp.ViewModels;
+
+namespace OfficeApp.Controllers;
+
+public class AccountController : Controller
+{
+
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel loginViewModel, string? returnUrl)
+    {
+
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.Username, loginViewModel.Password, true, false);
+
+            if (result.Succeeded)
+            {
+                if (returnUrl != null)
+                    return Redirect(returnUrl);
+
+                else
+                    return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Your credentials are either incorrect or we couldn't find you.");
+
+                return View(loginViewModel);
+            }
+        }
+        else
+        {
+            return View(loginViewModel);
+        }
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+
+        if (ModelState.IsValid)
+        {
+            IdentityUser identityUser = new IdentityUser
+            {
+                UserName = model.Username.Trim(),
+                Email = model.Username.Trim()
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout() 
+    {
+        await _signInManager.SignOutAsync();
+
+        return RedirectToAction(nameof(Login));
+    }
+}
